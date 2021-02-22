f266b8b2094b833cb2cf74ec8fa8be5008068940
Berestean Daniel <berestean97@mail.ru>
2021-02-22 10:37:08 +0200

    Resolved all Arrays exercices

--- Tony Ballantine tech 99 problems/src/ex3/Ex5.java
+++ Tony Ballantine tech 99 problems/src/ex3/Ex5.java
@@ -1,5 +1,25 @@
 package ex3;
-
+//3.5 Write a method that tests to see if an array is palindromic, i.e. the elements are the same when reversed.
 public class Ex5 {
 
+	static boolean is_pali(String[] arr) {
+		boolean is_true = true;
+		for (int i=0;i < (arr.length/2);i++) {
+			if(!arr[i].equals(arr[arr.length -i -1])) {
+				is_true = false;
+			}
+		}
+		return is_true;
+	}
+	
+	public static void main(String[] args) {
+		String [] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        String [] lonely = {"solo"};
+        String [] palindromic = {"Sausage", "Eggs", "Beans", "Beans", "Eggs", "Sausage"};
+		System.out.println(is_pali(breakfast));
+		System.out.println(is_pali(lonely));
+		System.out.println(is_pali(palindromic));
+		 
+	}
+	
 }
--- Tony Ballantine tech 99 problems/src/ex3/Ex6.java
+++ Tony Ballantine tech 99 problems/src/ex3/Ex6.java
@@ -1,5 +1,20 @@
 package ex3;
-
+//3.6 Write a method to print out an int array with consecutive duplicates eliminated
 public class Ex6 {
 
+	static void printarr(int[] arr) {
+		int temp=Integer.MIN_VALUE;
+		for (int i=0;i<arr.length;i++) {
+			if (arr[i]==temp) {
+				continue;
+			}
+			System.out.println(arr[i]);
+			temp =arr[i];
+		}
+	}
+	
+	public static void main (String[] args) {
+		int [] nums = {1,1,3,3,3,2,2,2,1,1,1,1,4,4,4,4};
+		printarr(nums);
+	}
 }
